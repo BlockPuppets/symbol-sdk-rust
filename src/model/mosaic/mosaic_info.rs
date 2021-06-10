07d6a121915879a186275dd2a0b6e5282a555d25
Puppet Black <blockpuppetblack0@gmail.com>
2021-06-09 22:14:19 -0500

    add MosaicInfo model

---
+++ src/model/mosaic/mosaic_info.rs
@@ -0,0 +1,84 @@
+/*
+ * // Copyright 2021 Developers of the Symbol sdk Rust project.
+ * //
+ * // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+ * // https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+ * // <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+ * // option. This file may not be copied, modified, or distributed
+ * // except according to those terms.
+ */
+
+
+use crate::account::Address;
+use crate::mosaic::{MosaicFlags, MosaicId};
+use crate::{Uint64, H192};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct MosaicInfo {
+    /// Version.
+    ///
+    pub version: u16,
+
+    /// The database record id.
+    ///
+    pub record_id: String,
+
+    /// The mosaic id.
+    ///
+    pub id: MosaicId,
+
+    /// The mosaic supply.
+    ///
+    pub supply: Uint64,
+
+    /// The block height were mosaic was created.
+    ///
+    pub start_height: Uint64,
+
+    /// The mosaic owner address.
+    ///
+    pub owner_address: Address<H192>,
+
+    /// The mosaic revision.
+    ///
+    pub revision: u16,
+
+    /// The mosaic flags.
+    ///
+    pub flags: MosaicFlags,
+
+    /// Mosaic divisibility.
+    ///
+    pub divisibility: u8,
+
+    /// Mosaic duration.
+    ///
+    pub duration: Uint64,
+}
+
+impl MosaicInfo {
+    /// Is mosaic supply mutable
+    ///
+    pub fn is_supply_mutable(&self) -> bool {
+        self.flags.supply_mutable
+    }
+
+    /// Is mosaic transferable
+    ///
+    pub fn is_transferable(&self) -> bool {
+        self.flags.transferable
+    }
+
+    /// Is mosaic restrictable
+    ///
+    pub fn is_restrictable(&self) -> bool {
+        self.flags.restrictable
+    }
+
+    /// Generate buffer.
+    ///
+    pub fn serialize() -> Vec<u8> {
+        todo!()
+    }
+}
