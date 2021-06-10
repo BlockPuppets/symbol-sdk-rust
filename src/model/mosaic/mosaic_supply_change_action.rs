5675a4713938303400f30942be63df539c0145ff
Puppet Black <blockpuppetblack0@gmail.com>
2021-06-09 22:14:39 -0500

    add mosaic_supply_change_action enum

---
+++ src/model/mosaic/mosaic_supply_change_action.rs
@@ -0,0 +1,18 @@
+/*
+ * // Copyright 2021 Developers of the Symbol sdk Rust project.
+ * //
+ * // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+ * // https://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+ * // <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+ * // option. This file may not be copied, modified, or distributed
+ * // except according to those terms.
+ */
+
+/// The supply type. Supported supply types are:
+/// 0: Increase in supply.
+/// 1: Decrease in supply.
+///
+pub enum MosaicSupplyChangeAction {
+    Decrease = 0x00,
+    Increase = 0x01,
+}
