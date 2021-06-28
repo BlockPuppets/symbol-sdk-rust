c0c2aa62d65a8dd58fc9d12114c3403c357d5d48
Puppet Black <blockpuppetblack0@gmail.com>
2021-06-27 22:01:04 -0500

    add transaction_version

--- src/model/transaction/mod.rs
+++ src/model/transaction/mod.rs
@@ -1,3 +1,5 @@
 pub use self::transaction_type::*;
+pub use self::transaction_version::*;
 
 mod transaction_type;
+mod transaction_version;
---
+++ src/model/transaction/transaction_version.rs
@@ -0,0 +1,120 @@
+use std::fmt;
+use std::ops::Deref;
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Serialize, Deserialize, Hash)]
+pub struct TransactionVersion(u8);
+
+impl TransactionVersion {
+    /// Transfer Transaction transaction version.
+    ///
+    pub const TRANSFER: TransactionVersion = TransactionVersion(1);
+
+    /// Register namespace transaction version.
+    ///
+    pub const NAMESPACE_REGISTRATION: TransactionVersion = TransactionVersion(1);
+
+    /// Mosaic definition transaction version.
+    ///
+    pub const MOSAIC_DEFINITION: TransactionVersion = TransactionVersion(1);
+
+    /// Mosaic supply change transaction version.
+    ///
+    pub const MOSAIC_SUPPLY_CHANGE: TransactionVersion = TransactionVersion(1);
+
+    /// Modify multisig account transaction version.
+    ///
+    pub const MULTISIG_ACCOUNT_MODIFICATION: TransactionVersion = TransactionVersion(1);
+
+    /// Aggregate complete transaction version.
+    ///
+    pub const AGGREGATE_COMPLETE: TransactionVersion = TransactionVersion(1);
+
+    /// Aggregate bonded transaction version.
+    ///
+    pub const AGGREGATE_BONDED: TransactionVersion = TransactionVersion(1);
+
+    /// Lock transaction version.
+    ///
+    pub const HASH_LOCK: TransactionVersion = TransactionVersion(1);
+
+    /// Secret Lock transaction version.
+    ///
+    pub const SECRET_LOCK: TransactionVersion = TransactionVersion(1);
+
+    /// Secret Proof transaction version.
+    ///
+    pub const SECRET_PROOF: TransactionVersion = TransactionVersion(1);
+
+    /// Address Alias transaction version.
+    ///
+    pub const ADDRESS_ALIAS: TransactionVersion = TransactionVersion(1);
+
+    /// Mosaic Alias transaction version.
+    ///
+    pub const MOSAIC_ALIAS: TransactionVersion = TransactionVersion(1);
+
+    /// Mosaic global restriction transaction version.
+    ///
+    pub const MOSAIC_GLOBAL_RESTRICTION: TransactionVersion = TransactionVersion(1);
+
+    /// Mosaic address restriction transaction version.
+    ///
+    pub const MOSAIC_ADDRESS_RESTRICTION: TransactionVersion = TransactionVersion(1);
+
+    /// Account Restriction address transaction version.
+    ///
+    pub const ACCOUNT_ADDRESS_RESTRICTION: TransactionVersion = TransactionVersion(1);
+
+    /// Account Restriction mosaic transaction version.
+    ///
+    pub const ACCOUNT_MOSAIC_RESTRICTION: TransactionVersion = TransactionVersion(1);
+
+    /// Account Restriction operation transaction version.
+    ///
+    pub const MODIFY_ACCOUNT_RESTRICTION_ENTITY_TYPE: TransactionVersion = TransactionVersion(1);
+
+    /// Link account transaction version.
+    ///
+    pub const ACCOUNT_KEY_LINK: TransactionVersion = TransactionVersion(1);
+
+    /// Account metadata transaction version.
+    ///
+    pub const ACCOUNT_METADATA: TransactionVersion = TransactionVersion(1);
+
+    /// Mosaic metadata transaction version.
+    ///
+    pub const MOSAIC_METADATA: TransactionVersion = TransactionVersion(1);
+
+    /// Namespace metadata transaction version.
+    ///
+    pub const NAMESPACE_METADATA: TransactionVersion = TransactionVersion(1);
+
+    /// Vrf key link transaction version.
+    ///
+    pub const VRF_KEY_LINK: TransactionVersion = TransactionVersion(1);
+
+    /// Voting key link transaction version.
+    ///
+    pub const VOTING_KEY_LINK: TransactionVersion = TransactionVersion(1);
+
+    /// Node key link transaction version.
+    ///
+    pub const NODE_KEY_LINK: TransactionVersion = TransactionVersion(1);
+
+    pub fn to_bytes(&self) -> [u8; 1] {
+        self.to_le_bytes()
+    }
+}
+
+impl Deref for TransactionVersion {
+    type Target = u8;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl fmt::Display for TransactionVersion {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
